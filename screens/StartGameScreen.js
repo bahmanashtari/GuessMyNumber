1f53759c67620e798ab5f214574874c911bf515c
bahmanashtari <bahman778@gmail.com>
2023-08-23 13:47:40 -0700

    add

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { TextInput, View, Alert, StyleSheet } from 'react-native'
 
 import PrimaryButton from '../components/PrimaryButton'
+import Colors from '../constants/colors'
 
 const StartGameScreen = ({ onPickedNumber }) => {
 	const [enteredNumber, setEnteredNumber] = useState('')
@@ -68,7 +69,7 @@ const styles = StyleSheet.create({
 		marginHorizontal: 24,
 		borderRadius: 8,
 		padding: 16,
-		backgroundColor: '#3b021f',
+		backgroundColor: Colors.primary800,
 		elevation: 4,
 		shadowColor: 'black',
 		shadowOffset: { width: 0, height: 2 },
@@ -79,9 +80,9 @@ const styles = StyleSheet.create({
 		height: 50,
 		width: 50,
 		fontSize: 32,
-		borderBottomColor: '#ddb52f',
+		borderBottomColor: Colors.accent500,
 		borderBottomWidth: 2,
-		color: '#ddb52f',
+		color: Colors.accent500,
 		marginVertical: 8,
 		fontWeight: 'bold',
 		textAlign: 'center',

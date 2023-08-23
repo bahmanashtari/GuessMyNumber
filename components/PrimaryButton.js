582a4d744828d20a358130c50f6c19aba43a27fb
bahmanashtari <bahman778@gmail.com>
2023-08-22 21:34:05 -0700

    add

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, StyleSheet } from 'react-native'
 
 const PrimaryButton = ({ children }) => {
+	const pressHandler = () => {
+		console.log('pressed')
+	}
+
 	return (
-		<View>
-			<Text>{children}</Text>
-		</View>
+		<Pressable onPress={pressHandler}>
+			<View style={styles.container}>
+				<Text>{children}</Text>
+			</View>
+		</Pressable>
 	)
 }
 
 export default PrimaryButton
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#72063c',
+		borderRadius: 28,
+		paddingVertical: 8,
+		paddingHorizontal: 16,
+		elevation: 2,
+	},
+})

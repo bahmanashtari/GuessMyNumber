bb76a4e7df8e62f0eda70ff202e46d0e4a0e4638
bahmanashtari <bahman778@gmail.com>
2023-08-22 21:26:25 -0700

    add

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,13 @@ import PrimaryButton from '../components/PrimaryButton'
 const StartGameScreen = () => {
 	return (
 		<View style={styles.inputContainer}>
-			<TextInput />
+			<TextInput
+				style={styles.numberInput}
+				maxLength={2}
+				keyboardType='number-pad'
+				autoCapitalize='none'
+				autoCorrect={false}
+			/>
 			<PrimaryButton>Reset</PrimaryButton>
 			<PrimaryButton>Confirm</PrimaryButton>
 		</View>
@@ -14,9 +20,27 @@ const StartGameScreen = () => {
 
 const styles = StyleSheet.create({
 	inputContainer: {
-		flex: 1,
 		marginTop: 100,
+		marginHorizontal: 24,
+		borderRadius: 8,
 		padding: 16,
+		backgroundColor: '#72063c',
+		elevation: 4,
+		shadowColor: 'black',
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 6,
+		shadowOpacity: 0.25,
+	},
+	numberInput: {
+		height: 50,
+		width: 50,
+		fontSize: 32,
+		borderBottomColor: '#ddb52f',
+		borderBottomWidth: 2,
+		color: '#ddb52f',
+		marginVertical: 8,
+		fontWeight: 'bold',
+		textAlign: 'center',
 	},
 })
 

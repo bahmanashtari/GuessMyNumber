63a22c308edd8e538892b645a872258717a79972
bahmanashtari <bahman778@gmail.com>
2023-08-24 13:41:41 -0700

    add

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { SafeAreaView, StyleSheet, ImageBackground } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 import { useFonts } from 'expo-font'
+import AppLoading from 'expo-app-loading'
 
 import StartGameScreen from './screens/StartGameScreen'
 import GameScreen from './screens/GameScreen'
@@ -12,10 +13,14 @@ export default function App() {
 	const [userNumber, setUserNumber] = useState()
 	const [gameIsOver, setGameIsOver] = useState(true)
 
-	useFonts({
+	const [fontsLoaded] = useFonts({
 		'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
 		'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
 	})
+
+	if (!fontsLoaded) {
+		return <AppLoading />
+	}
 
 	const pickedNumberHandler = pickedNumber => {
 		setUserNumber(pickedNumber)

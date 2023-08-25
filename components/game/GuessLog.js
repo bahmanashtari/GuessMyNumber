f5a01c6c6ce9103dcb58b48bb5e9a5b8a71db8c7
bahmanashtari <bahman778@gmail.com>
2023-08-25 00:39:39 -0700

    add

--- components/game/GuessLog.js
+++ components/game/GuessLog.js
@@ -1,16 +1,37 @@
-import { Text, StyleSheet } from 'react-native'
+import { useState, useEffect } from 'react'
+
+import { Text, View, StyleSheet } from 'react-native'
 
 import Colors from '../../constants/colors'
 
 const GuessLog = ({ children }) => {
-	return <Text style={styles.textOutput}>{children}</Text>
+	const [roundNumber, setRoundNumber] = useState(1)
+
+	useEffect(() => {
+		setRoundNumber(curr => curr + 1)
+	})
+
+	return (
+		<View style={styles.container}>
+			<View>
+				<Text style={styles.roundNumber}>{roundNumber}</Text>
+			</View>
+			<View>
+				<Text style={styles.guess}>{children}</Text>
+			</View>
+		</View>
+	)
 }
 
 export default GuessLog
 
 const styles = StyleSheet.create({
-	textOutput: {
+	container: {
+		flex: 'row',
+	},
+	guess: {
 		fontSize: 18,
 		color: Colors.accent500,
 	},
+	roundNumber: {},
 })
